402376dbf0f2321441c0b9ffc63a42621b77e586
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-07 14:02:10 +0530

    change

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.impute import SimpleImputer
+from sklearn.impute import SimpleImputer as si
 
 def main():
     os.system("clear")
@@ -13,6 +13,11 @@ def main():
     y = dataset.iloc[:, -1].values
     print(x)
     print(y)
+    imputer = si(missing_values=np.nan, strategy='median')
+    imputer.fit(x[:, 1:3])
+    x[:, 1:3]=imputer.transform(x[:, 1:3])
+    os.system("clear")
+    print(x)
 main()
 
 
