3925e549c5801b533632445b55d2284bb9fb06a6
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-08 10:42:15 +0530

    change

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -4,6 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer as si
+from sklearn.compose import ColumnTransformer 
+from sklearn.preprocessing import OneHotEncoder 
+from sklearn.preprocessing import LabelEncoder
+
+def encoding(x, y):
+  os.system("clear")
+  ct = ColumnTransformer(transformers=[
+  ('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+  
+  x = np.array(ct.fit_transform(x))
+  #print(x)
+
+  le = LabelEncoder()
+  y = le.fit_transform(y)
+  print(y)
 
 def main():
     os.system("clear")
@@ -13,11 +28,12 @@ def main():
     y = dataset.iloc[:, -1].values
     print(x)
     print(y)
-    imputer = si(missing_values=np.nan, strategy='median')
-    imputer.fit(x[:, 1:3])
-    x[:, 1:3]=imputer.transform(x[:, 1:3])
+    imputer = si(missing_values=np.nan, strategy='mean')
+    #imputer.fit(x[:, 1:3])
+    x[:, 1:3]=imputer.fit_transform(x[:, 1:3])
     os.system("clear")
     print(x)
+    encoding(x, y)
 main()
 
 
