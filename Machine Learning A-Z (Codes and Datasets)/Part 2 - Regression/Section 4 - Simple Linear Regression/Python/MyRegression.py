3f03c67dea81db9009683c4696742590ba3d673a
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-28 12:27:35 +0530

    Update MyRegression.py

--- Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 4 - Simple Linear Regression/Python/MyRegression.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 4 - Simple Linear Regression/Python/MyRegression.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 
 dataset = pd.read_csv("Salary_Data.csv")
-x = dataset.iloc[:, : -1]
+x = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
-
+print(y)
 #Splitting the Training Set and Test Set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.2, random_state=0)
@@ -14,3 +14,5 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.2, random
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
+
+# print(x_train)
