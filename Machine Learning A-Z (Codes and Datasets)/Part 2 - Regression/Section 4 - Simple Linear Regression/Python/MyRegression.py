139a5a76bc2f3ac23e506f47a73bd9441493d72f
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-28 16:00:30 +0530

    Update MyRegression.py

--- Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 4 - Simple Linear Regression/Python/MyRegression.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 4 - Simple Linear Regression/Python/MyRegression.py
@@ -1,36 +1,28 @@
-#Importing the libraries
-import numpy as np
-import pandas as pd
+#Inmporting the dataset and the required libraries
+from sklearn import datasets
 
-dataset = pd.read_csv("Salary_Data.csv")
-x = dataset.iloc[:, :-1]
-y = dataset.iloc[:, -1]
+boston = datasets.load_boston(return_X_y=False)
+x = boston.data
+y = boston.target
 
-#Splitting the Training Set and Test Set
+#Spilitting the dataset in the Training and Test Stage
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
 
-#Using Linear Regression
+# Performing the regression
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
-#predicting based on years of experience
-ypred = regressor.predict(x_test)
 
-#Making this interactive using Graph and MatplotLib
+y_pred = regressor.predict(x_train)
+print(f"Variance score is: {regressor.score(x_train, y_train)}")
+#Making the graph
+# Training Set
 import matplotlib.pyplot as plt
-#Showing Training set
-plt.scatter(x_train, y_train, color = 'red')
-plt.plot(x_train, regressor.predict(x_train), color = 'blue')
-plt.title('Salary Vs Experience (Training Set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
-
-#Showing Test Set
-plt.scatter(x_test, y_test, color = 'red')
-plt.plot(x_train, regressor.predict(x_train), color = 'blue')
-plt.title('Salary Vs Experience (Test Set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.scatter(regressor.predict(x_train), regressor.predict(x_train)-y_train, color = 'green')
+plt.scatter(regressor.predict(x_test), regressor.predict(x_test)-y_test, color='red')
+plt.hlines(y = 0, xmin = 0, xmax = 50, linewidth = 2)
+plt.legend(loc = 'upper right')
+#plt.plot(x_train, regressor.fit(x_train), color = 'blue')
+plt.title('Training Set')
 plt.show()
