fdd3df23191caf4119022e907ab6eeebe7006309
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-02-02 19:02:41 +0530

    change

--- Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 4 - Simple Linear Regression/Python/MyRegression.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 4 - Simple Linear Regression/Python/MyRegression.py
@@ -1,28 +1,40 @@
-#Inmporting the dataset and the required libraries
-from sklearn import datasets
+# #Inmporting the dataset and the required libraries
+# from sklearn import datasets
+#
+# boston = datasets.load_boston(return_X_y=False)
+# x = boston.data
+# y = boston.target
+#
+# #Spilitting the dataset in the Training and Test Stage
+# from sklearn.model_selection import train_test_split
+# x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
+#
+# # Performing the regression
+# from sklearn.linear_model import LinearRegression
+# regressor = LinearRegression()
+# regressor.fit(x_train, y_train)
+#
+# y_pred = regressor.predict(x_train)
+# print(f"Variance score is: {regressor.score(x_train, y_train)}")
+# #Making the graph
+# # Training Set
+# import matplotlib.pyplot as plt
+# plt.scatter(regressor.predict(x_train), regressor.predict(x_train)-y_train, color = 'green')
+# plt.scatter(regressor.predict(x_test), regressor.predict(x_test)-y_test, color='red')
+# plt.hlines(y = 0, xmin = 0, xmax = 50, linewidth = 2)
+# plt.legend(loc = 'upper right')
+# #plt.plot(x_train, regressor.fit(x_train), color = 'blue')
+# plt.title('Training Set')
+# plt.show()
+import numpy as np
+import pandas as pd
 
-boston = datasets.load_boston(return_X_y=False)
-x = boston.data
-y = boston.target
+dataset = pd.read_csv("Salary_Data.csv")
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
-#Spilitting the dataset in the Training and Test Stage
+#Training the dataset into test set and train set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
 
-# Performing the regression
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(x_train, y_train)
-
-y_pred = regressor.predict(x_train)
-print(f"Variance score is: {regressor.score(x_train, y_train)}")
-#Making the graph
-# Training Set
-import matplotlib.pyplot as plt
-plt.scatter(regressor.predict(x_train), regressor.predict(x_train)-y_train, color = 'green')
-plt.scatter(regressor.predict(x_test), regressor.predict(x_test)-y_test, color='red')
-plt.hlines(y = 0, xmin = 0, xmax = 50, linewidth = 2)
-plt.legend(loc = 'upper right')
-#plt.plot(x_train, regressor.fit(x_train), color = 'blue')
-plt.title('Training Set')
-plt.show()
+#Using Regression
